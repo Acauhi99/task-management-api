6a0d2cc22a02e253cad2e75a67f4e822ebfb737f
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-08-17 01:26:15 -0300

    :add: adionado validacoes aos dtos

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,9 +1,17 @@
+import { IsEmail, IsNumber, IsString, IsStrongPassword } from 'class-validator';
+
 export class CredentialDto {
+  @IsEmail()
   email: string;
+
+  @IsStrongPassword()
   password: string;
 }
 
 export class AuthResponseDto {
+  @IsString()
   token: string;
+
+  @IsNumber()
   expiresIn: number;
 }
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsOptional, IsStrongPassword, IsUUID } from 'class-validator';
+
 export class UserDto {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsEmail()
   email: string;
+
+  @IsStrongPassword()
   password: string;
 }
