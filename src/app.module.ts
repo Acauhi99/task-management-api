fb76f2e0f5343b9d2b24d18b12e2f8875a7ccb76
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-08-17 09:30:38 -0300

    :add: adicionado configs do db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
     TaskModule,
     UserModule,
     AuthModule,
+    DbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
